13f1c9c4384cf13c3dd85522cdc63b3fc8a274b8
Kasthuri Mudiyanselage Sanath <64699899+1998-sandunkasthuriarachchi@users.noreply.github.com>
2022-08-10 11:11:54 +0530

    logo/font/f-color

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar />
+            <div className="container">
+                <div class="row py-5">
+                    <div className="col-lg-6 col-md-6 col-sm-12 d-flex justify-content-between flex-column py-5">
+                        <h1>Text Field</h1>
+                        <h2>Text Field</h2>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-12">
+                        <img src="./logo.png" className="img-fluid" alt="" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-12 d-flex justify-content-center py-5">
+                        <a href="" className="text-decoration-none fs-3" style={{ color: '#98ff98' }}>
+                            Play
+                        </a>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-12 py-4 d-flex justify-content-center h-75">
+                        <p>will add unity dummy here</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div className="container">
+                <a href="" className="navbar-brand">
+                    <img src="./logo-32x32.png" alt="" />
+                </a>
+                <button
+                    className="navbar-toggler bg-light"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0 text-uppercase fw-bold">
+                        <li className="nav-item ">
+                            <a href="" className="nav-link active" aria-current="page" style={{ color: '#98ff98' }}>
+                                Play
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="" className="nav-link " aria-current="page" style={{ color: '#98ff98' }}>
+                                Info
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="" className="nav-link" aria-current="page" style={{ color: '#98ff98' }}>
+                                About
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="" className="nav-link" aria-current="page" style={{ color: '#98ff98' }}>
+                                Community
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
